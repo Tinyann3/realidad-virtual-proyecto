d1af237603554fceaeef0fcfe664a4a3767e0af6
joseameth2008-maker <joseameth2008@gmail.com>
2026-06-20 17:35:21 -0500

    creado el js

---
+++ script.js
@@ -0,0 +1,13 @@
+// "use strict" obliga a escribir código más limpio y evita errores silenciosos
+'use strict'; 
+
+// Esta línea espera a que todo el HTML se haya cargado antes de ejecutar el código
+document.addEventListener('DOMContentLoaded', () => {
+    
+    console.log("¡La página cargó y el JavaScript está conectado!");
+
+    // ==========================================
+    // AQUÍ EMPIEZA SU CÓDIGO DE JAVASCRIPT
+    // ==========================================
+
+});
